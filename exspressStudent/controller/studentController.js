d834819f1c4af7cfa68692a3364e5ed748f802bd
Elena <96632502+LenaVlasenko@users.noreply.github.com>
2022-08-10 21:20:10 +0300

    Update

--- exspressStudent/controller/studentController.js
+++ exspressStudent/controller/studentController.js
@@ -16,9 +16,36 @@ exports.create = function (request, response) {
     return response.status(201).json(newStudent);// вернули статус 201 новый студент
 }
 
-
-
-// метод read === GET
+// метод read All=== GET
 exports.index = function (request, response) {//получили всех студентов и отправили со статусом 201
     return response.status(200).json(students);
 };
+
+// метод read ONE === GET, вернуть конкретного
+exports.show = function (request, response) {   //получили всех студентов и отправили со статусом 201
+    //console.log(request.params.studentId)
+    let findId = request.params.studentId
+    let findStudent = students.find(s => s._id === findId)
+    console.log(findStudent)
+    if(!findStudent) {
+        //console.log('нет студента')
+        return response.status(404)
+    }
+    return response.status(200).json(findStudent);
+    //return response.status(200).json(students);
+};
+
+//update one === PUT
+exports.update = function (request, response) {
+    let findId = request.params.studentId
+    let findStudent = students.findIndex(s => s._id === findId)
+
+    console.log(findStudent)
+    if(findStudent === nul && typeof (findStudent) === undefined) {
+        console.log('нет студента')
+        return response.status(404)
+    }
+    students[findStudent] = request.body
+    console.log('Все обновляется')
+    return response.status(204).send()
+}
--- exspressStudent/routes/students.js
+++ exspressStudent/routes/students.js
@@ -6,7 +6,16 @@ let cStudents = require("./../controller/studentController")
 //создать новую запись === POST
 router.post("/", cStudents.create)
 
-//GET
+//read All === GET, корень
 router.get("/", cStudents.index)
 
+//read (one) === GET, на параметр
+router.get("/:studentId", cStudents.show)
+
+//read (one) === GET, на параметр
+router.put("/:studentId", cStudents.update)
+
+
+
+//CRUD
 module.exports = router;
