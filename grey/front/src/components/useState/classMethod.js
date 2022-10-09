e9b5ebe420d44ab2d0766fb3d488e65762add8c8
Elena <96632502+LenaVlasenko@users.noreply.github.com>
2022-10-09 21:25:07 +0300

    React ClassMethod

---
+++ grey/front/src/components/useState/classMethod.js
@@ -0,0 +1,52 @@
+
+import React from "react";
+
+export default class ClassMethod extends React.Component{
+
+    //Конструктор
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: localStorage.getItem('counter') || 0
+            // Мы тут описываем - какие данные у нас возможны
+        }
+        this.onChange = this.onChange.bind(this) // Разрешить методу доступ к классу
+    }
+
+    //Метод, который меняет состояние стейта
+    plus(){
+        const oldState = this.state
+        oldState.counter++
+        localStorage.setItem('counter', oldState.counter)
+        this.setState(oldState)
+    }
+    minus(){
+        const oldState = this.state
+        oldState.counter--
+        localStorage.setItem('counter', oldState.counter)
+        this.setState(oldState)
+    }
+
+    //Динамический перенос данных в стейт
+    onChange(e) {
+        const oldState = this.state
+        oldState[e.target.name] = e.target.value
+        localStorage.setItem(e.target.name, oldState.counter)
+        this.setState(oldState)
+    }
+
+
+    //Выводит на экран как выглядит компонент
+    render() {
+        return(
+            <>
+                {this.state.counter}
+                <input type="number"
+                       value={this.state.counter}
+                       onChange={this.onChange.bind(this)} name='counter'/>
+                <button onClick={this.plus.bind(this)} type="button">+</button>
+                <button onClick={this.minus.bind(this)} type="button">-</button>
+            </>
+        )
+    }
+}
