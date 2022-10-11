5960c6fb89d2a55424a459e78bede4fb5cd0e555
Elena <96632502+LenaVlasenko@users.noreply.github.com>
2022-10-11 21:05:13 +0300

    SelectNP

--- grey/front/src/components/novaPoshta/SelectNovaPoshta.js
+++ grey/front/src/components/novaPoshta/SelectNovaPoshta.js
@@ -1,20 +1,22 @@
 import {useState} from "react";
 
 
-export default function SelectNovaPoshta(){
+export default function SelectNovaPoshta(props){
+    // console.log("Props")
+    // console.log(props)
 
-
+    //ключ
     const key = "edcb75117704d71c9355d8f535288be4"
 
     //Место хранения областей
     const [areas, setAreas] = useState([]);
 
-    //Место получения областей
+    //Метод получения областей
     const loadAreas = () => {
         let data = {
             "apiKey": key,
             "modelName": "Address",
-            "calledMethod": "getAreas",
+            "calledMethod": "getAreas"
         }
 
         //появление данных
@@ -22,7 +24,6 @@ export default function SelectNovaPoshta(){
             method: 'POST',
             body: JSON.stringify(data)
         })
-
             .then(res => {
                 return res.json()
             })
@@ -34,32 +35,111 @@ export default function SelectNovaPoshta(){
                 console.log("err")
                 console.log(err)
             })
+    }
 
+    //Метод получения городов
+    const [cities, setCities] = useState([])
+    const changeSelectedArea=(ev) =>{
+        // console.log("User Select area: ")
+        // console.log(ev.target.value)
+
+        let data = {
+            "apiKey": key,
+            "modelName": "Address",
+            "calledMethod": "getCities",
+            "methodProperties": {
+                "AreaRef" :ev.target.value,
+            }
+        }
+
+        //появление данных
+        fetch('https://api.novaposhta.ua/v2.0/json/', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+            .then(res => {
+                return res.json()
+            })
+            .then(inCities => {
+                console.log(inCities)
+                setCities(inCities.data)
+            })
+            .catch(err=> {
+                console.log("err")
+                console.log(err)
+            })
+    }
+
+    //Метод получения отделений
+    const [warehouses, setWarehouses] = useState([])
+    const changeSelectedCity = (ev)=> {
+        // console.log("User Select area: ")
+        // console.log(ev.target.value)
+
+        let data = {
+            "apiKey": key,
+            "modelName": "Address",
+            "calledMethod": "getWarehouses",
+            "methodProperties": {
+                "CityRef" :ev.target.value,
+            }
+        }
+
+        fetch('https://api.novaposhta.ua/v2.0/json/', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+            .then(res => {
+                return res.json()
+            })
+            .then(inWarehouses => {
+                console.log(inWarehouses)
+                setWarehouses(inWarehouses.data)
+            })
+            .catch(err=> {
+                console.log("err")
+                console.log(err)
+            })
     }
 
 
 
     return(
         <>
-            {/*<button type="button" onClick={loadAreas}>load</button>*/}
+            <button type="button" onClick={loadAreas}>load</button>
+            <select onChange={changeSelectedArea}>
+                {areas.map(area => (
+                    <option key={area.Ref} value={area.Ref}>{area.Description}</option>
+                ))}
+            </select>
+            <select onChange={changeSelectedCity}>
+                {cities.map(city => (
+                    <option key={city.Ref} value={city.Ref}>{city.Description}</option>
+                ))}
+            </select>
+            <select>
+                {warehouses.map(warehouse => (
+                    <option key={warehouse.Ref} value={warehouse.Ref}>{warehouse.Description}</option>
+                ))}
+            </select>
 
-            <div className="btn-group">
-                <button type="button" className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown"
-                        aria-expanded="false" onClick={loadAreas}>
-                    load
-                </button>
+            {/*<div className="btn-group">*/}
+            {/*    <button type="button" className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown"*/}
+            {/*            aria-expanded="false" onClick={loadAreas}>*/}
+            {/*        load*/}
+            {/*    </button>*/}
 
-                <ul className="dropdown-menu">
-                    {areas.map(area =>(
-                        <li key={area.ref}>
-                            <a className="dropdown-item" href="#" >
-                                {area.Description}
-                            </a>
-                         </li>
-                    ))}
-                </ul>
+            {/*    <ul className="dropdown-menu">*/}
+            {/*        {areas.map(area =>(*/}
+            {/*            <li key={area.Ref}>*/}
+            {/*                <a className="dropdown-item" href="#" >*/}
+            {/*                    {area.Description}*/}
+            {/*                </a>*/}
+            {/*             </li>*/}
+            {/*        ))}*/}
+            {/*    </ul>*/}
 
-            </div>
+            {/*</div>*/}
         </>
     )
 }
