f506eb80018af426792fc502168a4b660cdcefe6
Elena <96632502+LenaVlasenko@users.noreply.github.com>
2022-11-05 14:02:33 +0200

    postFile

--- grey/back/controllers/api/files/FileController.js
+++ grey/back/controllers/api/files/FileController.js
@@ -1,25 +1,50 @@
 
 
-
 exports.getFile = function (request, response) {
-    console.log("Get Files")
-    let res = {} //обьект для ответа
+    console.log("Get File")
+    let res = {} // Объект для ответа
 
-    //Получаю мой файл
+    // Получаю мой файл
     let file = request.files.file;
-    // копирую файл в нужную мне папку
-    file.mv("./uploads/" + file.name);
+    // Копирую файл в нужную мне папку
+    console.log(file)
+    // Проблема - одинаковые имена файлов от разных пользователей
+
+
+    let uploadDir = './uploads/'
+
+    // Формирование каждому пользователю своей папки в хранилище
+    if (request.user) {
+        uploadDir+= request.user._id
+    } else {
+        uploadDir+= 'guest'
+    }
+
+    // console.log(request.user._id + " USER ")
+
+    // добавление папок год / месяц / число
+    let date = new Date();
+    uploadDir+= '/' + date.getFullYear();
+    uploadDir+= '/' + (1 + date.getMonth());
+    uploadDir+= '/' + date.getDate();
+
+    console.log(uploadDir)
+    // Изменение имени файла (случайное число, текущий момент времени, guid)
+    let fullPath = uploadDir + '/' + Date.now() + file.name
+    file.mv( fullPath);
 
     // Формирую ответ об успешной загрузке
     res = {
         status: "success",
         message: "File is uploaded",
         data: {
-            name: file.name,
+            oldName: file.name,
+            fullPath: fullPath,
             mimetype: file.mimetype,
             size: file.size,
         },
     }
+
 
     return response.status(200).json(res)
 }
--- grey/front/src/components/forms/FileForm.js
+++ grey/front/src/components/forms/FileForm.js
@@ -1,48 +1,44 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 
-
-
-export  default function FileForm(){
-
+export default function FileForm() {
     const { register, handleSubmit, reset, formState } = useForm()
     const { errors } = formState
 
 
     const onSubmit = async data => {
-        //обьект для отправки данных
+        // Объект для отправки данных на сервер
         const formData = new FormData();
-        //Если поле файла существует (заполненое)
+
+        // Если поле файлов существует (заполнено)
         if(data.file[0])
-            //ПРисоеденяем данные из файла в форму
+            // Присоединяем данные из файла в форму
             formData.append("file", data.file[0]);
         else {
-            // Если обьекта не существует - выводим сообщение об ошибке и уходим из отправки
-            //alert("NoFile");
+            // Если объекта не существует - выводим сообщение об ошибке и уходим из отправки
+            // alert("NoFile");
             errors.file = {}
             return;
         }
 
-
-        const res = await fetch("http://localhost:3333/api" + "/media/file/", {
+        const res = await fetch('http://localhost:3333/api' + '/media/file/', {
             method: "POST",
             body: formData,
+            headers: {
+                'authorization': localStorage.getItem('jwtToken')
+            }
         }).then((res) => res.json());
         console.log(res);
-        //alert(JSON.stringify(`${res.message}, status: ${res.status}`));
     };
-
 
     return (
         /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
         <form onSubmit={handleSubmit(onSubmit)}>
 
             <input type="file" {...register("file", { required: true })} />
-            {errors.file && <span><br/>This file is required<br/></span>}
-
+            {errors.file && <span><br/>Файл обязательное поле<br/></span>}
 
             <input type="submit" />
         </form>
     );
-
 }
