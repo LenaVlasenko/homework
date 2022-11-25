0875453f7d3f326cf6b87d778c12693e592a4b31
Elena <96632502+LenaVlasenko@users.noreply.github.com>
2022-11-25 20:06:20 +0200

    VueRefReactive

---
+++ vue/vue-project/src/components/Vars/RefReactive.vue
@@ -0,0 +1,34 @@
+<template>
+  <h3>Ref & Reactive</h3>
+  <div>
+    {{ v_reactive }}
+  </div>
+  <hr />
+  <div>
+    {{ v_ref }}
+  </div>
+  <hr />
+  <div>
+    {{ state }}
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from "vue";
+
+let someVars = "Hello World";
+
+let v_reactive = reactive(someVars);
+let v_ref = ref(someVars);
+
+const count = ref(0);
+const state = reactive({
+  count,
+});
+</script>
+
+<style scoped>
+h3 {
+  color: blue;
+}
+</style>
