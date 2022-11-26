fa482ccbd916aae7134a59ceec22a3f730aa24d6
Elena <96632502+LenaVlasenko@users.noreply.github.com>
2022-11-26 20:35:48 +0200

    PasvordVueValidSecond

--- vue/vue-project/src/components/MyPassword/ValidPass.vue
+++ vue/vue-project/src/components/MyPassword/ValidPass.vue
@@ -4,6 +4,10 @@
   <div>{{ computedByPass }}</div>
   <p>{{ textValid }}</p>
   <p>{{ textLetterValid }}</p>
+  <hr />
+  <p>{{ Validator.length }}</p>
+  <p>{{ Validator.valid }}</p>
+  <p>{{ Validator.letter }}</p>
 </template>
 
 <script setup>
@@ -25,6 +29,19 @@ const textLetterValid = computed(() => {
   return !password.text.match(/(?=.*[A-Z])(?=.*[0-9])/)
     ? "Пароль должен содержать одну большую букву и одну цифру"
     : "Все ок, super good";
+});
+
+const Validator = computed(() => {
+  return {
+    length:
+      password.text.length >= 6
+        ? "Добро пожаловать"
+        : "Пароль должен содержать не меньше 6 символов (computed)",
+    letter: !password.text.match(/(?=.*[A-Z])(?=.*[0-9])/)
+      ? "Пароль должен содержать одну большую букву и одну цифру"
+      : "Все ок, super good",
+    valid: password.text.length > 5 ? "valid" : "invalid",
+  };
 });
 </script>
 
